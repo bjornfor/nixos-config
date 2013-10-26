dc11878791ee880504db3817521774c6c2a7afda
Bjørn Forsman <bjorn.forsman@gmail.com>
2013-10-26 10:41:59 +0200

    Update bash prompt
    Make a two-line prompt:
    
      bfo@nixos-laptop ~
      $
    
    If logged in with ssh, hostname gets a different color (derived from the
    hostname).

--- configuration.nix
+++ configuration.nix
@@ -252,17 +252,26 @@ in
     #export PYTHONPATH=$PYTHONPATH:/run/current-system/sw/lib/python2.7/site-packages/
   '';
 
-  # Show git info in bash prompt. This is (mostly) the default NixOS prompt plus
-  # $(__git_ps1 " [git::%s]")
+  # Show git info in bash prompt and display a colorful hostname if using ssh.
   programs.bash.promptInit = ''
     export GIT_PS1_SHOWDIRTYSTATE=1
     source ${pkgs.gitAndTools.gitFull}/share/git/contrib/completion/git-prompt.sh
-    PROMPT_COLOR="1;31m"
-    let $UID && PROMPT_COLOR="1;32m"
-    PS1='\[\033[$PROMPT_COLOR\][\u@\h:\w$(__git_ps1 " [git::%s]")]\\$\[\033[0m\] '
-    if test "$TERM" = "xterm"; then
-      PS1="\[\033]2;\h:\u:\w\007\]$PS1"
+
+    __prompt_color="1;32m"
+    # Alternate color for hostname if the generated color clashes with prompt color
+    __alternate_color="1;33m"
+    __hostnamecolor="$__prompt_color"
+    # If logged in with ssh, pick a color derived from hostname
+    if [ -n "$SSH_CLIENT" ]; then
+      __hostnamecolor="1;$(${pkgs.nettools}/bin/hostname | od | tr ' ' '\n' | ${pkgs.gawk}/bin/awk '{total = total + $1}END{print 30 + (total % 6)}')m"
+      # Fixup color clash
+      if [ "$__hostnamecolor" = "$__prompt_color" ]; then
+        __hostnamecolor="$__alternate_color"
+      fi
     fi
+
+    PS1='\n\[\e[$__prompt_color\]\u@\[\e[$__hostnamecolor\]\]\h \[\e[$__prompt_color\]\w$(__git_ps1 " [git:%s]")\[\e[0m\]\n$ '
+
   '';
 
   programs.bash.enableCompletion = true;
