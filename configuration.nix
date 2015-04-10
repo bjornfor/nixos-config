9b52cc8f94ba3e9de01bd8efc5781a2c36ae4dd0
Bjørn Forsman <bjorn.forsman@gmail.com>
2015-04-10 22:14:44 +0200

    transmission-service: set rpc-bind-address = "0.0.0.0"
    That is actually the default value, but now it's explicit (and easy to
    change).

--- configuration.nix
+++ configuration.nix
@@ -697,6 +697,7 @@ in
         rpc-whitelist = "127.0.0.1,192.168.1.*";
         ratio-limit = 2;
         ratio-limit-enabled = true;
+        rpc-bind-address = "0.0.0.0";  # web server
       };
     };
 
