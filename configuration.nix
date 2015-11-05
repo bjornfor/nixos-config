e01b021c0c7814b1996503ffd31b77fe70511641
Bjørn Forsman <bjorn.forsman@gmail.com>
2015-11-05 21:49:51 +0100

    Add NFS export: /srv/nfs/wandboard/

--- configuration.nix
+++ configuration.nix
@@ -945,6 +945,7 @@ in
       enable = (hostname == myDesktop);
       exports = ''
         /nix/ 192.168.1.0/24(ro,subtree_check)
+        /srv/nfs/wandboard/ 192.168.1.0/24(rw,no_root_squash,no_subtree_check)
       '';
     };
 
