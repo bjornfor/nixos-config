dde0d9da8e09e37a610e699c1be1b0e6f2cde36e
Bjørn Forsman <bjorn.forsman@gmail.com>
2016-02-16 22:57:51 +0100

    Update comment header (copied from a fresh NixOS 15.09 install)

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,6 @@
 # Edit this configuration file to define what should be installed on
-# the system.  Help is available in the configuration.nix(5) man page
-# or the NixOS manual available on virtual console 8 (Alt+F8).
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, lib, pkgs, ... }:
 
