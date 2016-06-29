cb2c44800afe8ad38b6a014fc546a5da3fd08e75
Bjørn Forsman <bjorn.forsman@gmail.com>
2016-06-29 15:56:49 +0200

    Use nixpkgs.config instead of override for virtualbox.enableExtensionPack option
    Solves a future problem when I split configuration.nix into several
    modules as only one module can set boot.kernelPackages. Some machines
    might need the option (virtualbox.enableExtensionPack), some might not.
    As far as I can tell, boot.kernelPackages option cannot merge. And I
    don't want to use mkForce.

--- configuration.nix
+++ configuration.nix
@@ -115,11 +115,7 @@ in
   boot.kernelModules = [ "psmouse" ];
   boot.extraModprobeConfig = " options psmouse proto=imps ";
 
-  boot.kernelPackages = linuxPackages // {
-    #virtualbox = linuxPackages.virtualbox.override {
-    #  enableExtensionPack = (hostname == myDesktop);
-    #};
-  };
+  boot.kernelPackages = linuxPackages;
   boot.extraModulePackages = [ linuxPackages.lttng-modules ];
 
 
@@ -232,6 +228,7 @@ in
     allowUnfree = true;  # allow proprietary packages
     firefox.enableAdobeFlash = true;
     chromium.enablePepperFlash = true;
+    #virtualbox.enableExtensionPack = (hostname == myDesktop);
     packageOverrides = pkgs: {
       #qtcreator = pkgs.qtcreator.override { qt48 = pkgs.qt48Full; };
       #qemu = pkgs.qemu.override { spiceSupport = true; };
