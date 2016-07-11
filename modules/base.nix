732afd88a7ee6d797cba1d05da1ba3d9dbbe3c29
Bjørn Forsman <bjorn.forsman@gmail.com>
2016-07-11 11:13:18 +0200

    Split out users-and-groups.nix module from base.nix

--- modules/base.nix
+++ modules/base.nix
@@ -4,6 +4,7 @@
   imports = [
     # Include the results of the hardware scan.
     ../hardware-configuration.nix
+    ./users-and-groups.nix
   ];
 
   # List swap partitions activated at boot time.
@@ -29,52 +30,6 @@
   networking = {
     firewall.enable = false;
     networkmanager.enable = true;
-  };
-
-  users.extraUsers = {
-    bfo = {
-      description = "Bjørn Forsman";
-      uid = 1000;
-      extraGroups = [
-        "adm"
-        "audio"
-        "cdrom"
-        "dialout"
-        "docker"
-        "libvirtd"
-        "networkmanager"
-        "plugdev"
-        "scanner"
-        "systemd-journal"
-        "tracing"
-        "transmission"
-        "tty"
-        "usbtmc"
-        "vboxusers"
-        "video"
-        "wheel"
-        "wireshark"
-      ];
-      isNormalUser = true;
-      initialPassword = "initialpw";
-      # Subordinate user ids that user is allowed to use. They are set into
-      # /etc/subuid and are used by newuidmap for user namespaces. (Needed for
-      # LXC.)
-      subUidRanges = [
-        { startUid = 100000; count = 65536; }
-      ];
-      subGidRanges = [
-        { startGid = 100000; count = 65536; }
-      ];
-
-    };
-  };
-
-  users.extraGroups = {
-    plugdev = { gid = 500; };
-    tracing = { gid = 501; };
-    usbtmc = { gid = 502; };
-    wireshark = { gid = 503; };
   };
 
   nix = {
---
+++ modules/users-and-groups.nix
@@ -0,0 +1,48 @@
+{ config, lib, pkgs, ... }:
+{
+  users.extraUsers = {
+    bfo = {
+      description = "Bjørn Forsman";
+      uid = 1000;
+      extraGroups = [
+        "adm"
+        "audio"
+        "cdrom"
+        "dialout"
+        "docker"
+        "libvirtd"
+        "networkmanager"
+        "plugdev"
+        "scanner"
+        "systemd-journal"
+        "tracing"
+        "transmission"
+        "tty"
+        "usbtmc"
+        "vboxusers"
+        "video"
+        "wheel"
+        "wireshark"
+      ];
+      isNormalUser = true;
+      initialPassword = "initialpw";
+      # Subordinate user ids that user is allowed to use. They are set into
+      # /etc/subuid and are used by newuidmap for user namespaces. (Needed for
+      # LXC.)
+      subUidRanges = [
+        { startUid = 100000; count = 65536; }
+      ];
+      subGidRanges = [
+        { startGid = 100000; count = 65536; }
+      ];
+
+    };
+  };
+
+  users.extraGroups = {
+    plugdev = { gid = 500; };
+    tracing = { gid = 501; };
+    usbtmc = { gid = 502; };
+    wireshark = { gid = 503; };
+  };
+}
