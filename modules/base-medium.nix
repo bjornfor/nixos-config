b82e0600c6ae09a0591347234f5568f41062d78f
Bjørn Forsman <bjorn.forsman@gmail.com>
2016-09-19 12:47:41 +0200

    base-medium.nix: remove (duplicate) collectd config
    This is a duplicate of the one in mini.nix (and the config isn't
    generic, better have it in machine specific module).

--- modules/base-medium.nix
+++ modules/base-medium.nix
@@ -159,64 +159,6 @@
       };
     };
 
-    # TODO: Change perms on /var/lib/collectd from 700 to something more
-    # permissive, at least group readable?
-    # The NixOS service currently only sets perms *once*, so I've manually
-    # loosened it up for now, to allow lighttpd to read RRD files.
-    collectd = {
-      #enable = true;
-      extraConfig = ''
-        # Interval at which to query values. Can be overwritten on per plugin
-        # with the 'Interval' option.
-        # WARNING: You should set this once and then never touch it again. If
-        # you do, you will have to delete all your RRD files.
-        Interval 10
-
-        # Load plugins
-        LoadPlugin apcups
-        LoadPlugin contextswitch
-        LoadPlugin cpu
-        LoadPlugin df
-        LoadPlugin disk
-        LoadPlugin ethstat
-        LoadPlugin interface
-        LoadPlugin irq
-        LoadPlugin libvirt
-        LoadPlugin load
-        LoadPlugin memory
-        LoadPlugin network
-        LoadPlugin nfs
-        LoadPlugin processes
-        LoadPlugin rrdtool
-        LoadPlugin sensors
-        LoadPlugin tcpconns
-        LoadPlugin uptime
-
-        <Plugin "libvirt">
-          Connection "qemu:///system"
-        </Plugin>
-
-        # Ignore some paths/filesystems that cause "Permission denied" spamming
-        # in the log and/or are uninteresting or duplicates.
-        <Plugin "df">
-          MountPoint "/run/media/bfo/wd_apollo"
-          MountPoint "/var/lib/docker/devicemapper"
-          MountPoint "/nix/store"  # it's just a bind mount, already covered
-          FSType "fuse.gvfsd-fuse"
-          FSType "cgroup"
-          FSType "tmpfs"
-          FSType "devtmpfs"
-          IgnoreSelected true
-        </Plugin>
-
-        # Output/write plugin (need at least one, if metrics are to be persisted)
-        <Plugin "rrdtool">
-          CacheFlush 120
-          WritesPerSecond 50
-        </Plugin>
-      '';
-    };
-
     samba = {
       #enable = true;
       nsswins = true;
