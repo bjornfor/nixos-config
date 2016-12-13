51dffdcf29c98569e6bbee8c40e10834b03607e9
Bjørn Forsman <bjorn.forsman@gmail.com>
2016-12-13 21:19:45 +0100

    Enable WideVine in Chromium (for Netflix)

--- modules/base-small.nix
+++ modules/base-small.nix
@@ -106,6 +106,7 @@
     allowUnfree = true;  # allow proprietary packages
     firefox.enableAdobeFlash = true;
     chromium.enablePepperFlash = true;
+    chromium.enableWideVine = true;  # for Netflix
     packageOverrides = pkgs: {
       #qtcreator = pkgs.qtcreator.override { qt48 = pkgs.qt48Full; };
       #qemu = pkgs.qemu.override { spiceSupport = true; };
