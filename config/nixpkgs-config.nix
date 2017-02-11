e19ea41f145f054863220e8cb156c6f210657fea
Bjørn Forsman <bjorn.forsman@gmail.com>
2017-02-11 14:44:35 +0100

    nixpkgs-config.nix: add mtdutils-for-swupdate
    I used this to test swupdate on host (a year ago maybe).

--- config/nixpkgs-config.nix
+++ config/nixpkgs-config.nix
@@ -8,7 +8,22 @@
 
   packageOverrides = pkgs: {
     ltsa = pkgs.callPackage ../packages/ltsa/default.nix { };
+
+    mtdutils-for-swupdate = pkgs.mtdutils.overrideDerivation (args: rec {
+      # Copied from the .bbappend file from meta-swupdate.
+      postInstall = ''
+        mkdir -p "$out/lib"
+        mkdir -p "$out/include/mtd"
+        cp lib/libmtd.a "$out/lib"
+        cp ubi-utils/libubi*.a "$out/lib"
+        install -m 0644 ubi-utils/include/libubi.h $out/include/mtd/
+        install -m 0644 include/libmtd.h $out/include/mtd/
+        install -m 0644 include/mtd/ubi-media.h $out/include/mtd/
+      '';
+    });
+
     spotify-ripper = pkgs.callPackage ../packages/spotify-ripper/default.nix { };
+
     winusb = pkgs.callPackage ../packages/winusb/default.nix { };
   };
 }
