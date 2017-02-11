342a305cafb0e7b6438409200da6fe641ec8355f
Bjørn Forsman <bjorn.forsman@gmail.com>
2017-02-11 14:21:00 +0100

    Add winusb to custom-packages.nix

--- config/custom-packages.nix
+++ config/custom-packages.nix
@@ -4,7 +4,7 @@
   nixpkgs.config = {
     packageOverrides = pkgs: {
       inherit (pkgs.callPackages ../packages/default.nix { })
-        ltsa spotify-ripper;
+        ltsa spotify-ripper winusb;
     };
   };
 }
