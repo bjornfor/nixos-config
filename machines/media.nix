803fc136cf1f060ae727833bf687f93ad690128a
Bjørn Forsman <bjorn.forsman@gmail.com>
2017-10-10 21:49:18 +0200

    machines/media: send email notification if backup is too old
    Make use of the dieHook mechanism. Hide Marias email by storing it
    outside of the Nix store (/etc/marias-email-address.nix).

--- machines/media.nix
+++ machines/media.nix
@@ -80,6 +80,71 @@
     archiveBaseName = "maria-pc_seagate_expansion_drive_4tb";
     pathsToBackup = [ "/mnt/${archiveBaseName}" ];
     preHook = ''
+      # Email notification receivers.
+      # Separate multiple addresses with comma and space (", ").
+      to_recipients="$(cat /etc/marias-email-address.txt)"
+      cc_recipients="bjorn.forsman@gmail.com"
+
+      backup_age_in_days()
+      {
+          today=$(date +%Y-%m-%d)
+          newest_backup_date=$(borg info "$repository"::"$(borg list "$repository" | tail -1 | awk '{print $1}')" | awk '/Time \(start\)/ {print $4}')
+          # POSIX sh:
+          #n_days_old=$(echo "scale=0; ( $(date -d $today +%s) - $(date -d $newest_backup_date +%s) ) / (24*3600)" | bc)
+          # bash(?):
+          n_days_old=$(( ( $(date -d $today +%s) - $(date -d $newest_backup_date +%s) ) / (24*3600) ))
+          if [ "$n_days_old" -eq "$n_days_old" ] 2>/dev/null
+          then
+              # $n_days_old is an integer
+              echo "$n_days_old"
+          else
+              echo "-1"
+          fi
+      }
+
+      maybe_send_failure_notification()
+      {
+          n_days_old=$(backup_age_in_days)
+          echo "Last backup is $n_days_old days old"
+          if [ $n_days_old -ge 7 -a $(( $n_days_old % 7 )) = 0 ]; then
+              echo "Warning: backup is old ($n_days_old days), sending email"
+              send_email $(n_days_old)
+          fi
+      }
+
+      dieHook() {
+          maybe_send_failure_notification
+      }
+
+      send_email()
+      {
+          n_days_old=$1
+          test "$n_days_old" -eq "$n_days_old" || { echo "ERROR: Programming error, n_days_old=$n_days_old is not an integer"; exit 1; }
+          cat << EOM | sendmail -t
+      From: "Mr. Robot" <noreply>
+      To: $to_recipients
+      Cc: $cc_recipients
+      Subject: Obs, sikkerhetskopien din er gammel
+
+      Hei Maria,
+
+      Jeg heter Hal og en robot som Bjørn har laget.
+      Hver natt tar jeg sikkerhetskopi av dataene fra den eksterne
+      harddisken din og over til stue-PCen. Men nå er det $n_days_old dager siden sist.
+
+      Kan du la PCen stå på en natt slik at jeg får tatt en ny sikkerhetskopi for deg?
+
+      Ha en fin dag!
+
+      Mvh
+      Hal 9000 / Bjørn Forsman
+      EOM
+      }
+
+      # For test
+      #send_email $(backup_age_in_days)
+      #exit 0
+
       # access the mountpoint now, to trigger automount (why is this needed?)
       if ! ls -ld /mnt/${archiveBaseName}; then
           die "Failed to mount maria-pc"
