c97e783f7348b7de4463219500ad761c3574e0c1
Bjørn Forsman <bjorn.forsman@gmail.com>
2017-10-10 21:26:33 +0200

    base-medium.nix: enable PIA VPN with user/pass in /etc/pia-vpn.*
    I see no reason to not always activate it now. However, if the user/pass
    files don't exist there will be some complaining by the activation
    script :-)
    
      setting up /etc...
      /nix/store/HASH-nixos-system-*/activate: line 141: /etc/pia-vpn.password.wrong: No such file or directory
      /nix/store/HASH-nixos-system-*/activate: line 142: /etc/pia-vpn.password.wrong: No such file or directory
      [...]

--- config/base-medium.nix
+++ config/base-medium.nix
@@ -120,12 +120,9 @@
     xpra
   ];
 
-  # In a secret module, add this option:
-  #   networking.networkmanager.pia-vpn.username = "myuser";
-  # and optionally this (if not set, the OS keyring will be used):
-  #   networking.networkmanager.pia-vpn.password = "mypassword";
-  networking.networkmanager.pia-vpn.enable =
-    lib.mkIf (config.networking.networkmanager.pia-vpn.username != "") true;
+  networking.networkmanager.pia-vpn.enable = true;
+  networking.networkmanager.pia-vpn.usernameFile = "/etc/pia-vpn.username";
+  networking.networkmanager.pia-vpn.passwordFile = "/etc/pia-vpn.password";
   networking.networkmanager.pia-vpn.serverList =
     [ "denmark" "fi" "nl" "no" "sweden" "uk-london" "us-newyorkcity" ];
 
