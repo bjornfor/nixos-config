b343dcc51a48f03d2f0994a422383ac55ad0a664
Bjørn Forsman <bjorn.forsman@gmail.com>
2017-12-10 14:37:48 +0100

    mini.nix: add ddclient service

--- machines/mini.nix
+++ machines/mini.nix
@@ -59,6 +59,14 @@ in
 
   services = {
 
+    ddclient = {
+      enable = true;
+      # Use imperative configuration to keep secrets out of the (world
+      # readable) Nix store. If this option is not set, the NixOS options from
+      # services.ddclient.* will be used to populate /etc/ddclient.conf.
+      configFile = "/var/lib/ddclient/secrets/ddclient.conf";
+    };
+
     postfix = {
       domain = myDomain;
       hostname = myDomain;
