efefe2b3989eb620e2cb7dceeef865be52b8a894
Bjørn Forsman <bjorn.forsman@gmail.com>
2017-12-17 21:05:03 +0100

    apcupsd.nix: remove unneeded escape chars
    The NixOS module was fixed in 2015 with commit
    "nixos/apcupsd: don't evaluate event hooks at build time".
    
    (Actually, my APC Back-UPS ES 700 has been broken for quite some time, I
    think the configuration with the escape chars may actually not work at
    all.)

--- cfg/apcupsd.nix
+++ cfg/apcupsd.nix
@@ -4,16 +4,16 @@
   services.apcupsd = {
     enable = true;
     hooks.doshutdown = ''
-      HOSTNAME=\$(${pkgs.nettools}/bin/hostname)
-      printf \"Subject: apcupsd: \$HOSTNAME is shutting down\\n\" | ${pkgs.msmtp}/bin/msmtp -C /home/bfo/.msmtprc bjorn.forsman@gmail.com
+      HOSTNAME=$(${pkgs.nettools}/bin/hostname)
+      printf "Subject: apcupsd: $HOSTNAME is shutting down\n" | ${pkgs.msmtp}/bin/msmtp -C /home/bfo/.msmtprc bjorn.forsman@gmail.com
     '';
     hooks.onbattery = ''
-      HOSTNAME=\$(${pkgs.nettools}/bin/hostname)
-      printf \"Subject: apcupsd: \$HOSTNAME is running on battery\\n\" | ${pkgs.msmtp}/bin/msmtp -C /home/bfo/.msmtprc bjorn.forsman@gmail.com
+      HOSTNAME=$(${pkgs.nettools}/bin/hostname)
+      printf "Subject: apcupsd: $HOSTNAME is running on battery\n" | ${pkgs.msmtp}/bin/msmtp -C /home/bfo/.msmtprc bjorn.forsman@gmail.com
     '';
     hooks.offbattery = ''
-      HOSTNAME=\$(${pkgs.nettools}/bin/hostname)
-      printf \"Subject: apcupsd: \$HOSTNAME is running on mains power\\n\" | ${pkgs.msmtp}/bin/msmtp -C /home/bfo/.msmtprc bjorn.forsman@gmail.com
+      HOSTNAME=$(${pkgs.nettools}/bin/hostname)
+      printf "Subject: apcupsd: $HOSTNAME is running on mains power\n" | ${pkgs.msmtp}/bin/msmtp -C /home/bfo/.msmtprc bjorn.forsman@gmail.com
     '';
     configText = ''
       UPSTYPE usb
