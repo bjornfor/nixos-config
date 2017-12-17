aafea1832942bc50d57f6a7dc8e1ad00aa4e2610
Bjørn Forsman <bjorn.forsman@gmail.com>
2017-12-17 21:10:36 +0100

    apcupsd.nix: send mail to root
    Separation of concern. (Who gets root mail is set in
    services.postfix.rootAlias.)

--- cfg/apcupsd.nix
+++ cfg/apcupsd.nix
@@ -5,15 +5,15 @@
     enable = true;
     hooks.doshutdown = ''
       HOSTNAME=$(${pkgs.nettools}/bin/hostname)
-      printf "Subject: apcupsd: $HOSTNAME is shutting down\n" | ${pkgs.msmtp}/bin/msmtp -C /home/bfo/.msmtprc bjorn.forsman@gmail.com
+      printf "Subject: apcupsd: $HOSTNAME is shutting down\n" | /run/wrappers/bin/sendmail root
     '';
     hooks.onbattery = ''
       HOSTNAME=$(${pkgs.nettools}/bin/hostname)
-      printf "Subject: apcupsd: $HOSTNAME is running on battery\n" | ${pkgs.msmtp}/bin/msmtp -C /home/bfo/.msmtprc bjorn.forsman@gmail.com
+      printf "Subject: apcupsd: $HOSTNAME is running on battery\n" | /run/wrappers/bin/sendmail root
     '';
     hooks.offbattery = ''
       HOSTNAME=$(${pkgs.nettools}/bin/hostname)
-      printf "Subject: apcupsd: $HOSTNAME is running on mains power\n" | ${pkgs.msmtp}/bin/msmtp -C /home/bfo/.msmtprc bjorn.forsman@gmail.com
+      printf "Subject: apcupsd: $HOSTNAME is running on mains power\n" | /run/wrappers/bin/sendmail root
     '';
     configText = ''
       UPSTYPE usb
