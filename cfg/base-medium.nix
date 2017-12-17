27bf561b0a5e8cc99264bf5266884cda4e0b40cb
Bjørn Forsman <bjorn.forsman@gmail.com>
2017-12-17 16:25:55 +0100

    Factor out cfg/apcupsd.nix

--- cfg/base-medium.nix
+++ cfg/base-medium.nix
@@ -5,6 +5,7 @@
     # Include the results of the hardware scan.
     ../hardware-configuration.nix
     ./base-small.nix
+    ./apcupsd.nix
     ./munin.nix
     ./desktop-gnome3.nix
     ./virtualisation.nix
@@ -227,29 +228,6 @@
       # (after 'modprobe usbmon').
       SUBSYSTEM=="usbmon", GROUP="usbmon", MODE="640"
     '';
-
-    apcupsd = {
-      #enable = true;
-      hooks.doshutdown = ''
-        HOSTNAME=\$(${pkgs.nettools}/bin/hostname)
-        printf \"Subject: apcupsd: \$HOSTNAME is shutting down\\n\" | ${pkgs.msmtp}/bin/msmtp -C /home/bfo/.msmtprc bjorn.forsman@gmail.com
-      '';
-      hooks.onbattery = ''
-        HOSTNAME=\$(${pkgs.nettools}/bin/hostname)
-        printf \"Subject: apcupsd: \$HOSTNAME is running on battery\\n\" | ${pkgs.msmtp}/bin/msmtp -C /home/bfo/.msmtprc bjorn.forsman@gmail.com
-      '';
-      hooks.offbattery = ''
-        HOSTNAME=\$(${pkgs.nettools}/bin/hostname)
-        printf \"Subject: apcupsd: \$HOSTNAME is running on mains power\\n\" | ${pkgs.msmtp}/bin/msmtp -C /home/bfo/.msmtprc bjorn.forsman@gmail.com
-      '';
-      configText = ''
-        UPSTYPE usb
-        NISIP 127.0.0.1
-        BATTERYLEVEL 75
-        MINUTES 10
-        #TIMEOUT 10  # for debugging, shutdown after N seconds on batteries
-      '';
-    };
 
     samba = {
       #enable = true;
