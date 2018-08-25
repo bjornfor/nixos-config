54f3b8b421dd091f7e5077e8e1ded6c106b0de19
Bjørn Forsman <bjorn.forsman@gmail.com>
2018-08-25 08:15:53 +0200

    desktop-gnome3.nix: add extensions: dash-to-dock, system-monitor
    Note that they must (additionally) be manually enabled in GNOME Tweaks.

--- cfg/desktop-gnome3.nix
+++ cfg/desktop-gnome3.nix
@@ -12,4 +12,12 @@
       libinput.enable = true;
     };
   };
+
+  environment.systemPackages = with pkgs; [
+    # Extensions must be manually enabled in GNOME Tweaks (previously named
+    # Tweak Tool). Adding them here only makes them available, but not active.
+    gnomeExtensions.dash-to-dock
+    gnomeExtensions.system-monitor
+  ];
+
 }
