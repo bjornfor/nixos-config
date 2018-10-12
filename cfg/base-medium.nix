43fd19bcf28cd7c4a810559d11687788f04b2378
Bjørn Forsman <bjorn.forsman@gmail.com>
2018-10-12 21:43:37 +0200

    syncthing: allow ACLs on /var/lib/syncthing
    Add a workaround to undo the 700 perms syncthing sets on
    /var/lib/syncthing on startup. Without this the ACL entries for my user
    stop working.

--- cfg/base-medium.nix
+++ cfg/base-medium.nix
@@ -298,4 +298,17 @@
           "${statusEmail} root %i";
     };
   };
+
+  # Undo the 700 perms syncthing sets on /var/lib/syncthing on startup. Without
+  # this the ACL entries for my user stop working.
+  # See
+  #  https://github.com/syncthing/syncthing/issues/3434
+  #  https://github.com/NixOS/nixpkgs/issues/47513
+  systemd.services.syncthing.postStart = lib.mkForce ''
+    sleep 10
+    chmod g+rx "${config.services.syncthing.dataDir}"
+  '';
+
+  # Enable debug trace to spot permission errors.
+  #systemd.services.syncthing.environment.STTRACE = "scanner";
 }
