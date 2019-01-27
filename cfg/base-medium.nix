a31f850f810da2f12d06e363e2d63c57082d596e
Bjørn Forsman <bjorn.forsman@gmail.com>
2019-01-27 11:57:07 +0100

    postfix: force ipv4
    Postfix (or my system) seems to prefer ipv6 now, but that breaks on
    my network:
    
      connect to gmail-smtp-in.l.google.com[2a00:1450:4010:c09::1b]:25: Network is unreachable
    
    So let's force ipv4.

--- cfg/base-medium.nix
+++ cfg/base-medium.nix
@@ -202,6 +202,14 @@
       rootAlias = "bjorn.forsman@gmail.com";
       extraConfig = ''
         inet_interfaces = loopback-only
+
+        # Postfix (or my system) seems to prefer ipv6 now, but that breaks on
+        # my network:
+        #
+        #   connect to gmail-smtp-in.l.google.com[2a00:1450:4010:c09::1b]:25: Network is unreachable
+        #
+        # So let's force ipv4.
+        smtp_address_preference = ipv4
       '';
     };
 
