04d739b3915758fdfdcb87bceab77f6cfcce3508
Bjørn Forsman <bjorn.forsman@gmail.com>
2019-02-04 08:34:13 +0100

    pkgs: remove mtdutils-for-swupdate
    Not needed anymore.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -16,18 +16,5 @@
 
   ltsa = pkgs.callPackage ./ltsa/default.nix { };
 
-  mtdutils-for-swupdate = pkgs.mtdutils.overrideDerivation (args: rec {
-    # Copied from the .bbappend file from meta-swupdate.
-    postInstall = ''
-      mkdir -p "$out/lib"
-      mkdir -p "$out/include/mtd"
-      cp lib/libmtd.a "$out/lib"
-      cp ubi-utils/libubi*.a "$out/lib"
-      install -m 0644 ubi-utils/include/libubi.h $out/include/mtd/
-      install -m 0644 include/libmtd.h $out/include/mtd/
-      install -m 0644 include/mtd/ubi-media.h $out/include/mtd/
-    '';
-  });
-
   winusb = pkgs.callPackage ./winusb/default.nix { };
 }
