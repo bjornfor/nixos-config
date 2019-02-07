6d9e9761627d7699245cd7527d925a7d76f57c64
Bjørn Forsman <bjorn.forsman@gmail.com>
2019-02-07 13:21:32 +0100

    Factor out cfg/syncthing.nix from base-medium.nix

--- cfg/base-medium.nix
+++ cfg/base-medium.nix
@@ -6,6 +6,7 @@
     ./munin.nix
     ./desktop-gnome3.nix
     ./dictionary.nix
+    ./syncthing.nix
     ./virtualisation.nix
   ];
 
@@ -213,11 +214,6 @@
       '';
     };
 
-    syncthing = {
-      enable = true;
-      group = "syncthing"; # NixOS defaults to "nogroup" (should be fixed)
-    };
-
     # Provide "MODE=666" or "MODE=664 + GROUP=plugdev" for a bunch of USB
     # devices, so that we don't have to run as root.
     udev.packages = with pkgs; [
@@ -307,17 +303,4 @@
           "${statusEmail} root %i";
     };
   };
-
-  # Undo the 700 perms syncthing sets on /var/lib/syncthing on startup. Without
-  # this the ACL entries for my user stop working.
-  # See
-  #  https://github.com/syncthing/syncthing/issues/3434
-  #  https://github.com/NixOS/nixpkgs/issues/47513
-  systemd.services.syncthing.postStart = lib.mkForce ''
-    sleep 10
-    chmod g+rx "${config.services.syncthing.dataDir}"
-  '';
-
-  # Enable debug trace to spot permission errors.
-  #systemd.services.syncthing.environment.STTRACE = "scanner";
 }
---
+++ cfg/syncthing.nix
@@ -0,0 +1,21 @@
+{ config, lib, pkgs, ... }:
+
+{
+  services.syncthing = {
+    enable = true;
+    group = "syncthing"; # NixOS defaults to "nogroup" (should be fixed)
+  };
+
+  # Undo the 700 perms syncthing sets on /var/lib/syncthing on startup. Without
+  # this the ACL entries for my user stop working.
+  # See
+  #  https://github.com/syncthing/syncthing/issues/3434
+  #  https://github.com/NixOS/nixpkgs/issues/47513
+  systemd.services.syncthing.postStart = lib.mkForce ''
+    sleep 10
+    chmod g+rx "${config.services.syncthing.dataDir}"
+  '';
+
+  # Enable debug trace to spot permission errors.
+  #systemd.services.syncthing.environment.STTRACE = "scanner";
+}
