f20fbc4f1165ef86c2851cb69fce8a98a1d051b4
Bjørn Forsman <bjorn.forsman@gmail.com>
2019-02-07 09:38:37 +0100

    base-small: import ../options/cifs-user-mount.nix

--- cfg/base-small.nix
+++ cfg/base-small.nix
@@ -9,6 +9,7 @@
     ./users-and-groups.nix
 
     ../options/borg-backup.nix
+    ../options/cifs-user-mount.nix
     ../options/collectd-graph-panel.nix
     ../options/gitolite-mirror.nix
     ../options/nextcloud.nix
