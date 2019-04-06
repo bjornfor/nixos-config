75cd788433653c8baa55d337505d31f96ab803e7
Bjørn Forsman <bjorn.forsman@gmail.com>
2019-04-06 22:47:22 +0200

    default.nix: build custom iso
    The custom bit is starting sshd on boot and allowing two ssh keys access
    to the system.

--- default.nix
+++ default.nix
@@ -4,10 +4,33 @@
 
 let
   nixosFunc = import (pkgs.path + "/nixos");
+
+  iso =
+    (nixosFunc {
+      configuration = {
+        imports = [
+          (pkgs.path + "/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")
+          #(pkgs.path + "/nixos/modules/installer/cd-dvd/installation-cd-graphical-kde.nix")
+
+          # Provide an initial copy of the NixOS channel so that the user
+          # doesn't need to run "nix-channel --update" first.
+          (pkgs.path + "/nixos/modules/installer/cd-dvd/channel.nix")
+        ];
+
+        # Activate SSH at boot
+        systemd.services.sshd.wantedBy = pkgs.lib.mkForce [ "multi-user.target" ];
+        users.users.root.openssh.authorizedKeys.keys = with import ./misc/ssh-keys.nix; [
+          mini.bf.default
+          whitetip.bf.default
+        ];
+      };
+    }).config.system.build.isoImage;
+
   buildConfig = config:
     (nixosFunc { configuration = config; }) // { recurseForDerivations = true; };
 in
 {
+  inherit iso;
   hydra = buildConfig ./machines/hydra/configuration.nix;
   media = buildConfig ./machines/media/configuration.nix;
   mini = buildConfig ./machines/mini/configuration.nix;
