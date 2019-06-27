785ec6357ab43184c1af4efa2e3810278e2522cd
Bjørn Forsman <bjorn.forsman@gmail.com>
2019-06-27 08:03:49 +0200

    cfg/bcache.nix: only apply config if bcache kernel module is available
    I think the glob patterns makes this module safe to include even on
    systems without bcache, but this feels better.

--- cfg/bcache.nix
+++ cfg/bcache.nix
@@ -1,17 +1,18 @@
 { config, lib, pkgs, ... }:
 
 {
-  systemd.tmpfiles.rules = [
-    # Backing device
-    "w /sys/block/bcache*/bcache/sequential_cutoff - - - - ${builtins.toString (256*1024*1024)}" # default: 4MiB
-    "w /sys/block/bcache*/bcache/cache_mode - - - - writeback" # default: writethrough
-    "w /sys/block/bcache*/bcache/writeback_percent - - - - 30" # default: 10
+  systemd.tmpfiles.rules =
+    if builtins.elem "bcache" config.boot.initrd.availableKernelModules then [
+      # Backing device
+      "w /sys/block/bcache*/bcache/sequential_cutoff - - - - ${builtins.toString (256*1024*1024)}" # default: 4MiB
+      "w /sys/block/bcache*/bcache/cache_mode - - - - writeback" # default: writethrough
+      "w /sys/block/bcache*/bcache/writeback_percent - - - - 30" # default: 10
 
-    # Cache device.
-    # Prevent reads and writes going to the hdd when the ssd is slow, because
-    # using this feature doesn't work well in my experience. Things become even
-    # slower.
-    "w /sys/fs/bcache/*/congested_read_threshold_us  - - - - 0" # default: 2000
-    "w /sys/fs/bcache/*/congested_write_threshold_us - - - - 0" # default: 20000
-  ];
+      # Cache device.
+      # Prevent reads and writes going to the hdd when the ssd is slow, because
+      # using this feature doesn't work well in my experience. Things become even
+      # slower.
+      "w /sys/fs/bcache/*/congested_read_threshold_us  - - - - 0" # default: 2000
+      "w /sys/fs/bcache/*/congested_write_threshold_us - - - - 0" # default: 20000
+    ] else [];
 }
