09999d664f584907a0ea12c65141e804df836519
Bjørn Forsman <bjorn.forsman@gmail.com>
2019-07-21 14:00:43 +0200

    desktop-gnome3.nix: add gnomeExtensions.gsconnect

--- cfg/desktop-gnome3.nix
+++ cfg/desktop-gnome3.nix
@@ -12,6 +12,7 @@
     # Extensions must be manually enabled in GNOME Tweaks (previously named
     # Tweak Tool). Adding them here only makes them available, but not active.
     gnomeExtensions.dash-to-dock
+    gnomeExtensions.gsconnect
     gnomeExtensions.nohotcorner
     gnomeExtensions.system-monitor
   ];
