ac84260267d92e068ef9c3785c4a408fb349b67f
Bjørn Forsman <bjorn.forsman@gmail.com>
2019-09-06 17:16:44 +0200

    pkgs/custom-desktop-entries: simplify all entries with makeSimpleWebApp

--- pkgs/custom-desktop-entries/default.nix
+++ pkgs/custom-desktop-entries/default.nix
@@ -27,76 +27,54 @@ let
 
   entries = {
 
-  get-nett-tv = makeDesktopItem {
-    name = "get-nett-tv";  # nix store path name
+  get-nett-tv = makeSimpleWebApp {
+    server = "tv.get.no";
     # Chromium fails with
     #   This video file cannot be played.
     #   (Error Code: 102630)
     # so use google-chrome instead.
-    exec = "google-chrome-stable --app=https://tv.get.no/";
+    browser = "google-chrome-stable";
     icon = fetchurl {
       name = "get-nett-tv-favicon.png";
       url = "https://tv.get.no/favicon.png";
       sha256 = "0acrlix5qmnb32zaqcch0khb6s7ajrw4qv5ikx33bpf71mimkc94";
     };
     comment = "Get Nett-TV";
-    desktopName = "Get Nett-TV";
-    #categories = "Network;WebBrowser";
-    extraEntries = ''
-      StartupWMClass=tv.get.no
-    '';
   };
 
-  gmail = makeDesktopItem {
-    name = "gmail";  # nix store path name
-    exec = "chromium-browser --app=https://mail.google.com/";
+  gmail = makeSimpleWebApp {
+    server = "mail.google.com";
     icon = fetchurl {
       url = "https://upload.wikimedia.org/wikipedia/commons/a/ab/Gmail_Icon.svg";
       sha256 = "1avrc2laqmviih3gx4pkxrd7v2hkcgp48c7zcb1wmxbnxvcqxgqr";
     };
-    comment = "Web Mail";
-    desktopName = "GMail";
-    extraEntries = ''
-      StartupWMClass=mail.google.com
-    '';
+    comment = "GMail";
   };
 
-  netflix = makeDesktopItem {
-    name = "netflix";  # nix store path name
+  netflix = makeSimpleWebApp {
+    server = "www.netflix.com";
     # It's a pain to maintain widewine for Chromium (slow to build, breaks), so
     # use google-chrome for netflix.
-    exec = "google-chrome-stable --app=https://www.netflix.com/";
+    browser = "google-chrome-stable";
     icon = fetchurl {
       url = "http://www.iconarchive.com/download/i106070/papirus-team/papirus-apps/netflix.svg";
       sha256 = "06n3crmfc3k8yahybic399p832vzj5afrdqvlizrk8lbk3plrjd2";
     };
-    comment = "Watch TV series and movies online";
-    desktopName = "Netflix";
-    #categories = "Network;WebBrowser";
-    extraEntries = ''
-      StartupWMClass=www.netflix.com
-    '';
+    comment = "Netflix";
   };
 
-  nrk-tv = makeDesktopItem {
-    name = "nrk-tv";  # nix store path name
-    exec = "chromium-browser --app=https://tv.nrk.no/";
+  nrk-tv = makeSimpleWebApp {
+    server = "tv.nrk.no";
     icon = fetchurl {
       name = "nrk-tv-logo.png";
       url = "http://mirrors.kodi.tv/addons/leia/plugin.video.nrk/icon.png";
       sha256 = "0a0cn831qcn1wn2zqrgjhw3q3ch9li7fqgazvcii4a8gcrvcc3sm";
     };
     comment = "NRK TV";
-    desktopName = "NRK TV";
-    #categories = "Network;WebBrowser";
-    extraEntries = ''
-      StartupWMClass=tv.nrk.no
-    '';
   };
 
-  sbanken = makeDesktopItem {
-    name = "sbanken";  # nix store path name
-    exec = "chromium-browser --app=https://sbanken.no/";
+  sbanken = makeSimpleWebApp {
+    server = "sbanken.no";
     # icon from google play
     # (https://play.google.com/store/apps/details?id=no.skandiabanken)
     icon = fetchurl {
@@ -104,26 +82,16 @@ let
       url = "https://lh3.googleusercontent.com/qY0PzdGykNPdmbLmHQKGYAesB7CgmXO-bqCJdI957RRMZ57p82BME081WcSgDCH4OSQ=s180";
       sha256 = "1n4mjyhjn3npc785bvb3r0cpwndrzdzq8qq60d93hbgh9y04mdda";
     };
-    comment = "Online bank";
-    desktopName = "Sbanken";
-    extraEntries = ''
-      StartupWMClass=sbanken.no
-    '';
+    comment = "Sbanken";
   };
 
-  youtube = makeDesktopItem {
-    name = "youtube";  # nix store path name
-    exec = "chromium-browser --app=https://www.youtube.com/";
+  youtube = makeSimpleWebApp {
+    server = "www.youtube.com";
     icon = fetchurl {
       url = "https://upload.wikimedia.org/wikipedia/commons/4/40/Youtube_icon.svg";
       sha256 = "0gqnp61pbcsfd34w6r9bjxnpzkrlb0nhwb8z3h2a4xbyawa9dpcq";
     };
     comment = "YouTube";
-    desktopName = "YouTube";
-    #categories = "Network;WebBrowser";
-    extraEntries = ''
-      StartupWMClass=www.youtube.com
-    '';
   };
 
 };
