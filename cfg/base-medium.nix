daa4ecc12768e2a26828c7945a1c29f3f37e8dfc
Bjørn Forsman <bjorn.forsman@gmail.com>
2020-02-16 09:19:49 +0100

    cfg/base-medium.nix: add nix-bisect
    It's not yet packaged in nixpkgs.

--- cfg/base-medium.nix
+++ cfg/base-medium.nix
@@ -101,6 +101,16 @@
     pythonPackages.ipython
     pythonPackages.sympy
     python2nix
+    (python3.pkgs.buildPythonPackage rec {
+      pname = "nix-bisect";
+      version = "0.2.0";
+      src = pkgs.fetchFromGitHub {
+        owner = "timokau";
+        repo = "nix-bisect";
+        rev = "v${version}";
+        sha256 = "0rg7ndwbn44kximipabfbvvv5jhgi6vs87r64wfs5by81iw0ivam";
+      };
+    })
     ripgrep
     rls
     rmlint
