e73abb20ff771cc52e05984fc95c84fa711f6cba
Bjørn Forsman <bjorn.forsman@gmail.com>
2020-02-16 09:24:29 +0100

    default.nix: pin nixpkgs by default
    Pin the nixpkgs used for checking the configuration of all machines.
    Also parameterize the expression so it's easy to switch between branches
    / channels:
    
      nix-build [--argstr branch master] ...
    
    Eventually I want to set up CI builds for this repo, and then I cannot
    depend on $NIX_PATH.

--- default.nix
+++ default.nix
@@ -1,6 +1,49 @@
 # Sanity check the repo by evaluating/building all machine configs.
 
-{ pkgs ? import <nixpkgs> {} }:
+let
+  nixpkgsGitUrl = "https://github.com/NixOS/nixpkgs.git";
+
+  # TODO: Check if 'versionOlder version "20.03"' behaves differently when
+  # using channels or git trees due to the "pre-git" suffix from lib.version
+  # when using git trees. If it differs, fix it.
+  branches = {
+    # a snapshot of stable
+    pinned = builtins.fetchGit {
+      url = nixpkgsGitUrl;
+      ref = "refs/heads/nixos-19.09";
+      rev = "ea553d8c67c6a718448da50826ff5b6916dc9c59"; # 2020-02-03
+    };
+    release = builtins.fetchGit {
+      url = nixpkgsGitUrl;
+      ref = "refs/heads/release-19.09";
+    };
+    master = builtins.fetchGit {
+      url = nixpkgsGitUrl;
+      ref = "refs/heads/master";
+    };
+    # TODO: channels include programs.sqlite (for shell command-not-found
+    # functionality), but command-not-found.pl hardcodes that path (no
+    # $NIX_PATH lookup).
+    stable-channel = builtins.fetchTarball {
+      url = "https://nixos.org/channels/nixos-19.09/nixexprs.tar.xz";
+    };
+    unstable-channel = builtins.fetchTarball {
+      url = "https://nixos.org/channels/nixpkgs-unstable/nixexprs.tar.xz";
+    };
+  };
+
+  badBranchMsg = branch:
+    let
+      branchNames = builtins.toString (builtins.attrNames branches);
+    in
+      "unsupported branch \"${branch}\", valid branch names: ${branchNames}";
+
+in
+
+{ branch ? "pinned"  # default to pinned/reproducible
+, nixpkgs ? branches."${branch}" or (throw (badBranchMsg branch))
+, pkgs ? let p = import nixpkgs { config = {}; overlays = []; }; in builtins.trace "nixpkgs version: ${p.lib.version} (rev: ${nixpkgs.rev or "unknown"})" p
+}:
 
 let
   nixosFunc = import (pkgs.path + "/nixos");
