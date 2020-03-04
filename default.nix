e533fbe10248513394f4fc546ca6ef8e79425c2d
Bjørn Forsman <bjorn.forsman@gmail.com>
2020-03-04 13:33:46 +0100

    default.nix: skip vmWithBootLoader to make everything buildable
    I like to check everything with 'nix-build', but I'm blocked by
    machines.$MACHINE.vmWithBootLoader breaking when
    boot.loader.system-boot.enable is true. Work around by skipping that
    attribute since it's an upstream issue that's existed for a long time
    (unfortunately).
    
    The 'vm' attribute still works though, so NixOS configs can still be
    tested in a VM.

--- default.nix
+++ default.nix
@@ -79,15 +79,20 @@ let
     }).config.system.build.isoImage;
 
   buildConfig = config:
-    (nixosFunc { configuration = config; }) // { recurseForDerivations = true; };
+    # FIXME: Machines that set 'boot.loader.systemd-boot.enable = true' fail to
+    # build the vmWithBootloader attribute, ref
+    # https://github.com/NixOS/nixpkgs/pull/65133.
+    pkgs.lib.filterAttrs
+    (n: v: if n == "vmWithBootLoader"
+           then pkgs.lib.warn "FIXME: skipping vmWithBootLoader" false
+           else true
+    )
+    ((nixosFunc { configuration = config; }) // { recurseForDerivations = true; });
 in
 {
   inherit iso;
   localPkgs = import ./pkgs/default.nix { inherit pkgs; };
   machines = pkgs.recurseIntoAttrs {
-    # FIXME: Machines that set 'boot.loader.systemd-boot.enable = true' fail to
-    # build the vmWithBootloader attribute, ref
-    # https://github.com/NixOS/nixpkgs/pull/65133.
     media = buildConfig ./machines/media/configuration.nix;
     mini = buildConfig ./machines/mini/configuration.nix;
     srv1 = buildConfig ./machines/srv1/configuration.nix;
