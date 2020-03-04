7a846566b7e1703f1d0d0975401b7b7e6d298c5c
Bjørn Forsman <bjorn.forsman@gmail.com>
2020-03-04 20:22:57 +0100

    Factor out options/module-list.nix

--- cfg/base-small.nix
+++ cfg/base-small.nix
@@ -7,12 +7,7 @@
     ./keyboard.nix
     ./shell.nix
 
-    ../options/borg-backup.nix
-    ../options/cifs-user-mount.nix
-    ../options/collectd-graph-panel.nix
-    ../options/gitolite-mirror.nix
-    ../options/nextcloud.nix
-    ../options/pia/pia-nm.nix
+    ../options/module-list.nix
   ];
 
   # List swap partitions activated at boot time.
---
+++ options/module-list.nix
@@ -0,0 +1,10 @@
+{
+  imports = [
+    ../options/borg-backup.nix
+    ../options/cifs-user-mount.nix
+    ../options/collectd-graph-panel.nix
+    ../options/gitolite-mirror.nix
+    ../options/nextcloud.nix
+    ../options/pia/pia-nm.nix
+  ];
+}
