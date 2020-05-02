ac12b70c01a1bea345c98a6f3b64aee44f414009
Bjørn Forsman <bjorn.forsman@gmail.com>
2020-05-02 11:28:27 +0200

    cfg/shell.nix: use starship instead of custom prompt
    It has built-in support for "how long did that command take"!

--- cfg/shell.nix
+++ cfg/shell.nix
@@ -91,26 +91,7 @@
     source "${pkgs.fzf}/share/fzf/key-bindings.bash"
   '';
 
-  # Show git info in bash prompt and display a colorful hostname if using ssh.
   programs.bash.promptInit = ''
-    export GIT_PS1_SHOWDIRTYSTATE=1
-    source ${pkgs.gitAndTools.gitFull}/share/git/contrib/completion/git-prompt.sh
-
-    __prompt_color="1;32m"
-    # Alternate color for hostname if the generated color clashes with prompt color
-    __alternate_color="1;33m"
-    __hostnamecolor="$__prompt_color"
-    # If logged in with ssh, pick a color derived from hostname
-    if [ -n "$SSH_CLIENT" ]; then
-      __hostnamecolor="1;$(${pkgs.nettools}/bin/hostname | od | tr ' ' '\n' | ${pkgs.gawk}/bin/awk '{total = total + $1}END{print 30 + (total % 6)}')m"
-      # Fixup color clash
-      if [ "$__hostnamecolor" = "$__prompt_color" ]; then
-        __hostnamecolor="$__alternate_color"
-      fi
-    fi
-
-    __red="1;31m"
-
-    PS1='\n$(ret=$?; test $ret -ne 0 && printf "\[\e[$__red\]$ret\[\e[0m\] ")\[\e[$__prompt_color\]\u@\[\e[$__hostnamecolor\]\h \[\e[$__prompt_color\]\w$(__git_ps1 " [git:%s]")\[\e[0m\]\n$ '
+    eval "$(${pkgs.starship}/bin/starship init bash)"
   '';
 }
