6a35790310f41d85a52110eeea8f626df580ea4c
Bjørn Forsman <bjorn.forsman@gmail.com>
2020-06-02 09:13:10 +0200

    Factor out livecd from ./default.nix to ./isos/livecd/default.nix

--- default.nix
+++ default.nix
@@ -8,36 +8,7 @@
 let
   nixosFunc = import (pkgs.path + "/nixos");
 
-  iso =
-    (nixosFunc {
-      configuration = {
-        imports = [
-          (pkgs.path + "/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")
-          #(pkgs.path + "/nixos/modules/installer/cd-dvd/installation-cd-graphical-gnome.nix")
-          #(pkgs.path + "/nixos/modules/installer/cd-dvd/installation-cd-graphical-kde.nix")
-
-          # Provide an initial copy of the NixOS channel so that the user
-          # doesn't need to run "nix-channel --update" first.
-          (pkgs.path + "/nixos/modules/installer/cd-dvd/channel.nix")
-        ];
-
-        # Activate SSH at boot
-        systemd.services.sshd.wantedBy = pkgs.lib.mkForce [ "multi-user.target" ];
-        users.users.root.openssh.authorizedKeys.keys = with import ./misc/ssh-keys.nix; [
-          mini.bf.default
-          whitetip.bf.default
-        ];
-
-        isoImage.contents = [
-          { source = pkgs.writeText "readme"
-              ''
-                Custom NixOS installer built from @bjornfor's nixos-config git repo.
-              '';
-            target = "/README.txt";
-          }
-        ];
-      };
-    }).config.system.build.isoImage;
+  iso = import ./isos/livecd/default.nix { inherit pkgs; };
 
   buildConfig = config:
     # FIXME: Machines that set 'boot.loader.systemd-boot.enable = true' fail to
---
+++ isos/livecd/default.nix
@@ -0,0 +1,38 @@
+# A NixOS live CD with ssh enabled.
+
+{ pkgs }:
+
+let
+  nixosFunc = import (pkgs.path + "/nixos");
+
+  iso = (nixosFunc {
+    configuration = {
+      imports = [
+        (pkgs.path + "/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")
+        #(pkgs.path + "/nixos/modules/installer/cd-dvd/installation-cd-graphical-gnome.nix")
+        #(pkgs.path + "/nixos/modules/installer/cd-dvd/installation-cd-graphical-kde.nix")
+
+        # Provide an initial copy of the NixOS channel so that the user
+        # doesn't need to run "nix-channel --update" first.
+        (pkgs.path + "/nixos/modules/installer/cd-dvd/channel.nix")
+      ];
+
+      # Activate SSH at boot
+      systemd.services.sshd.wantedBy = pkgs.lib.mkForce [ "multi-user.target" ];
+      users.users.root.openssh.authorizedKeys.keys = with import ../../misc/ssh-keys.nix; [
+        mini.bf.default
+        whitetip.bf.default
+      ];
+
+      isoImage.contents = [
+        { source = pkgs.writeText "readme"
+            ''
+              Custom NixOS installer built from @bjornfor's nixos-config git repo.
+            '';
+          target = "/README.txt";
+        }
+      ];
+    };
+  }).config.system.build.isoImage;
+in
+  iso
